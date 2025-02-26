c3b82b5fb91b7669f1d65d9f7df571d0db2c4b4b
Serhii Saliei <70771776+sergeysaley@users.noreply.github.com>
2025-02-26 16:07:08 +0200

    Update ch_serial3.py
    Unused cases were removed

--- ch_serial3.py
+++ ch_serial3.py
@@ -94,12 +94,7 @@ class lists:
     ]
 
     com_valid=[
-    "n1auto", "n2auto", "n3auto", "n1on", "n2on", "n3on", "n1off", "n2off", "n3off", 
-    "ne1auto", "ne3auto", "ne1on", "ne3on", "ne1off", "ne3off", 
-    "ns1auto", "ns3auto", "ns1on", "ns3on", "ns1off", "ns3off", 
-    "s1auto", "s2auto", "s3auto", "s1on", "s2on", "s3on", "s1off", "s2off", "s3off", 
-    "se1auto", "se3auto", "se1on", "se3on", "se1off", "se3off", 
-    "ss1auto", "ss3auto", "ss1on", "ss3on", "ss1off", "ss3off"]
+    "n1auto", "n2auto", "n3auto", "n1on", "n2on", "n3on", "n1off", "n2off", "n3off"]
 
 #reading incoming bytes on serial
 def read_serial(q):
@@ -236,25 +231,13 @@ def read_status():
             break
         else:
             bypass = 'undefined'
-            if (rx[9] == '8a' or rx[9] == '87'): bypass = 'bypass: auto; '
-            if (rx[9] == 'aa' or rx[9] == 'a7'): bypass = 'bypass: on; '
-            if (rx[9] == 'ca' or rx[9] == 'c7'): bypass = 'bypass: off; '
+            if (rx[9] == '8e'): bypass = 'bypass: auto; '
+            if (rx[9] == 'ae'): bypass = 'bypass: on; '
+            if (rx[9] == 'ce'): bypass = 'bypass: off; '
             if (rx[13] == '00' or rx[13] == '20'):
                 if rx[10] == '0c': mode = 'mode: normal; speed: 1; '
                 if rx[10] == '12': mode = 'mode: normal; speed: 2; '
                 if rx[10] == '21': mode = 'mode: normal; speed: 3; '
-                if rx[10] == '4a': mode = 'mode: normal exhaust; speed: 1; '
-                if rx[10] == '51': mode = 'mode: normal exhaust; speed: 3; '
-                if rx[10] == '94': mode = 'mode: normal supply; speed: 1; '
-                if rx[10] == 'a2': mode = 'mode: normal supply; speed: 3; '
-            if rx[13] == '10':
-                if rx[10] == '0c': mode = 'mode: save; speed: 1; '
-                if rx[10] == '12': mode = 'mode: save; speed: 2; '
-                if rx[10] == '21': mode = 'mode: save; speed: 3; '
-                if rx[10] == '4a': mode = 'mode: save exhaust; speed: 1; '
-                if rx[10] == '51': mode = 'mode: save exhaust; speed: 3; '
-                if rx[10] == '94': mode = 'mode: save supply; speed: 1; '
-                if rx[10] == 'a2': mode = 'mode: save supply; speed: 3; '
         status = mode + bypass + status
         break
     
@@ -278,47 +261,19 @@ def run_com(cm):
             rx[2] = '00'
             rx[3] = 'a0'
             if cm[0] == 'off': 
-                if rx[9] == '8a': rx[9] = '0a'
-                if rx[9] == 'aa': rx[9] = '2a'
-                if rx[9] == 'ca': rx[9] = '4a'
-
-                if rx[9] == '87': rx[9] = '07'
-                if rx[9] == 'a7': rx[9] = '27'
-                if rx[9] == 'c7': rx[9] = '47'
-            elif (cm[0] == 'rhoff' and (rx[9] == '8a' or rx[9] == 'aa' or rx[9] == 'ca')):
-                print ('use rhoff')
-                rx[11] = '40'
-            elif (cm[0] == 'rhon' and (rx[9] == '8a' or rx[9] == 'aa' or rx[9] == 'ca')):
-                print ('use rhon')
-                rx[11] = 'd0'
+                if rx[9] == '8e': rx[9] = '0e'
+                if rx[9] == 'ae': rx[9] = '2e'
+                if rx[9] == 'ce': rx[9] = '4e'
             else:
-                if (cm[2] == 'auto' and rx[9] == '0a'): rx[9] = '8a'        #'bypass: auto; '
-                if (cm[2] == 'on' and rx[9] == '2a'):   rx[9] = 'aa'        #'bypass: on; '
-                if (cm[2] == 'off' and rx[9] == '4a'):  rx[9] = 'ca'        #'bypass: off; '
-                if (cm[2] == 'auto' and rx[9] == '07'): rx[9] = '87'        #'bypass: auto; '
-                if (cm[2] == 'on' and rx[9] == '27'):   rx[9] = 'a7'        #'bypass: on; '
-                if (cm[2] == 'off' and rx[9] == '47'):  rx[9] = 'c7'        #'bypass: off; '
+                if (cm[2] == 'auto' and rx[9] == '0e'): rx[9] = '8e'        #'bypass: auto; '
+                if (cm[2] == 'on' and rx[9] == '2e'):   rx[9] = 'ae'        #'bypass: on; '
+                if (cm[2] == 'off' and rx[9] == '4e'):  rx[9] = 'ce'        #'bypass: off; '
 
-                if (cm[0] == 'n' or cm[0] == 'ne' or cm[0] == 'ns'):
+                if (cm[0] == 'n'):
                     rx[13] = '20'
-                    #rx[13] = '00'
                     if (cm[0] == 'n' and cm[1] == '1'): rx[10] = '0c'         #'mode: normal; speed: 1; '
                     if (cm[0] == 'n' and cm[1] == '2'): rx[10] = '12'         #'mode: normal; speed: 2; '
-                    if (cm[0] == 'n' and cm[1] == '3'): rx[10] = '21'         #'mode: normal; speed: 3; '
-                    if (cm[0] == 'ne' and cm[1] == '1'): rx[10] = '4a'        #'mode: normal exhaust; speed: 1; '
-                    if (cm[0] == 'ne' and cm[1] == '3'): rx[10] = '51'        #'mode: normal exhaust; speed: 3; '
-                    if (cm[0] == 'ns' and cm[1] == '1'): rx[10] = '94'        #'mode: normal supply; speed: 1; '
-                    if (cm[0] == 'ns' and cm[1] == '3'): rx[10] = 'a2'        #'mode: normal supply; speed: 3; '
-                if (cm[0] == 's' or cm[0] == 'se' or cm[0] == 'ss'):
-                    rx[13] = '10'
-                    if (cm[0] == 's' and cm[1] == '1'): rx[10] = '0c'         #'mode: save; speed: 1; '
-                    if (cm[0] == 's' and cm[1] == '2'): rx[10] = '12'         #'mode: save; speed: 2; '
-                    if (cm[0] == 's' and cm[1] == '3'): rx[10] = '21'         #'mode: save; speed: 3; '
-                    if (cm[0] == 'se' and cm[1] == '1'): rx[10] = '4a'        #'mode: save exhaust; speed: 1; '
-                    if (cm[0] == 'se' and cm[1] == '3'): rx[10] = '51'        #'mode: save exhaust; speed: 3; '
-                    if (cm[0] == 'ss' and cm[1] == '1'): rx[10] = '94'        #'mode: save supply; speed: 1; '
-                    if (cm[0] == 'ss' and cm[1] == '3'): rx[10] = 'a2'        #'mode: save supply; speed: 3; '
-        
+                    if (cm[0] == 'n' and cm[1] == '3'): rx[10] = '21'         #'mode: normal; speed: 3; '        
         del rx[16]
         
         packet = HexToByte(''.join(rx))
@@ -381,7 +336,7 @@ def main():
                 st = time.time() 
                 print ()
                 i += 1
-        if (sys.argv[1] == 'off' or sys.argv[1] == 'rhoff' or sys.argv[1] == 'rhon'):
+        if (sys.argv[1] == 'off'):
             run_com([sys.argv[1], ' ', ' '])
             sys.exit()
         if sys.argv[1] == 'help':
